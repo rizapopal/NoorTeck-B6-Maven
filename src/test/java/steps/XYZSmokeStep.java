46f63ceb191e4f1e73b3af49c702dd181c12078e
rizapopal <rizapopal@macbook-pro>
2022-05-19 20:38:08 -0400

    Maven committ

---
+++ src/test/java/steps/XYZSmokeStep.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+public class XYZSmokeStep {
+
+	@Given("User navigates to login page")
+	public void user_navigates_to_login_page() {
+	  System.out.println("User is able to login");
+	}
+}
---
+++ src/test/resources/orangehrm/SmokeTest.feature
@@ -0,0 +1,4 @@
+Feature: Desciption of feature/functionality we are testing
+
+  Scenario: Verify user is able to login sucessfully
+    Given User navigates to login page
